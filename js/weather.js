2e23813f7311bd03e730a7fdabdb42a2dc469ec2
zuckerco <Ryguy360@gmail.com>
2016-11-21 18:40:08 -0500

    Giphy is now set up.
    Added Giphy API stuff. For now the search query is hard coded as
    “Sunny,” and we don’t want the query to be like that. We need to figure
    out how to have OWM send the weather description to Giphy to query. To
    do that, I’m pretty sure we need to figure out how to turn the OWM
    description into a variable that can be appended to the end of the
    Giphy API web call.

--- js/weather.js
+++ js/weather.js
@@ -1,3 +1,10 @@
+// Giphy api variables
+var apiG = "http://api.giphy.com/v1/gifs/search?";
+var apiKeyG = "&api_key=dc6zaTOxFJmzC";
+var query = "&q=sunny";
+
+
+// Open Weather Map variables
 var weather;
 var api= 'http://api.openweathermap.org/data/2.5/weather?q='
 var apiKey = '&APPID=79b31a94c523013cc25062224cf470ca';
@@ -11,7 +18,7 @@ function setup (){
   var button = select('#submit');
   button.mousePressed(weatherAsk);
 
-  input = select('#city');  
+  input = select('#city');
 }
 
 function weatherAsk(){
@@ -24,12 +31,26 @@ function gotData(data){
   weather = data;
 }
 
+// Draws two circles in the browser that represent the temp and humidity
 function draw(){
   background(0);
   if (weather){
     var temp = weather.main.temp
     var humidity = weather.main.humidity
+    var description = weather.weather.description
     ellipse(100, 100, temp, temp);
     ellipse(300, 100, humidity, humidity);
   }
 }
+
+// // This is where the GIFs come from
+
+// function setup(){
+//   noCanvas();
+//   var url = apiG + apiKeyG + description;
+//   loadJSON(url, gotData);
+// }
+//
+// function gotData(giphy){
+//   createImg(giphy.data[0].images.original.url);
+// }

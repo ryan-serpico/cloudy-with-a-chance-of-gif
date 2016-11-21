674d2c6edcd76957600fefaed2e5f25ef95bb570
zuckerco <Ryguy360@gmail.com>
2016-11-20 22:14:12 -0500

    Users can now input cities.
    Besides adding the ability for users to type in their own cities, I
    also added a few necessary javascript libraries.

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,22 @@
 var weather;
+var api= 'http://api.openweathermap.org/data/2.5/weather?q='
+var apiKey = '&APPID=79b31a94c523013cc25062224cf470ca';
+var units = '&units=imperial';
+
+var input;
+
 function setup (){
   createCanvas (400,200);
-  loadJSON('http://api.openweathermap.org/data/2.5/weather?q=Miami&APPID=79b31a94c523013cc25062224cf470ca&units=imperial',gotData);
+
+  var button = select('#submit');
+  button.mousePressed(weatherAsk);
+
+  input = select('#city');  
+}
+
+function weatherAsk(){
+  var url = api + input.value() + apiKey + units;
+  loadJSON(url, gotData);
 }
 
 function gotData(data){
@@ -12,7 +27,9 @@ function gotData(data){
 function draw(){
   background(0);
   if (weather){
-    ellipse(100, 100, weather.main.temp, weather.main.temp);
-    ellipse(300, 100, weather.main.humidity, weather.main.humidity);
+    var temp = weather.main.temp
+    var humidity = weather.main.humidity
+    ellipse(100, 100, temp, temp);
+    ellipse(300, 100, humidity, humidity);
   }
 }

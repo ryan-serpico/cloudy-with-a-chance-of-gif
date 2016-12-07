e0f35ae335dacf592a3f18acef7f4425b68f36dc
zuckerco <ryan@Ryans-MacBook-Pro.local>
2016-12-06 22:48:44 -0500

    It's alive!
    At long last, I have the two APIs talking to each other. I figured out
    how to use a callback to get the Giphy API to wait for the user to type
    in their search.
    
    Additionally, I have made it so that every time the user searches, it
    gives them a random gif. I also added a basic Bootstrap theme.
    
    Looking forward, issues include styling the gif (due to the fact the
    gif is not in the HTML), search suggestions and the fact that the user
    has to reload the page every time they want to do a new search.

--- js/weather.js
+++ js/weather.js
@@ -4,71 +4,48 @@ var api= 'http://api.openweathermap.org/data/2.5/weather?q='
 var apiKey = '&APPID=79b31a94c523013cc25062224cf470ca';
 var units = '&units=imperial';
 
-// Giphy api variables
-var apiG = "http://api.giphy.com/v1/gifs/search?";
-var apiKeyG = "&api_key=dc6zaTOxFJmzC";
-var query = "&q=";
+// Giphy API Stuff
+var apiG = "http://api.giphy.com/v1/gifs/random?";
+var apiKeyG = "api_key=dc6zaTOxFJmzC";
+var query = "&tag=";
 var description = ""
 var button = ""
 
 var input;
 
-function preload (){
-  createCanvas (400,200);
-
+function setup(){
   button = select('#submit');
   button.mousePressed(weatherAsk);
 
   input = select('#city');
 }
 
+// Weather API called
 function weatherAsk(){
   var url = api + input.value() + apiKey + units;
-  loadJSON(url, gotData);
+  loadJSON(url, banana);
 }
 
-function gotData(data){
-//  println(data);
+function banana(data){
   weather = data;
 }
 
-// Draws two circles in the browser that represent the temp and humidity
+//Weather Data is acquired
 function draw(){
-  // background(0);
   if (weather){
-    // var temp = weather.main.temp;
-    // alert(temp);
-    // var humidity = weather.main.humidity;
-    // ellipse(100, 100, temp, temp);
-    // ellipse(300, 100, humidity, humidity);
   description = weather.weather[0].description;
-  // for loop - loop through the var description as if it's an array
-  // var newDescription = "";
-  /*for (var i = 0; i < description.length; i++) {
-    console.log(description[i]);
-    if description[i] is a space, change to +
-    but you need to write each char into newdescription, one at a time
-    newDescription = thing1 + thing2 + thing3; -----> to do one at a time, you can do newDescription += description[i]
-  }
-  */
-
-
-
-   alert(description);
    noLoop();
-
-  // description = newDescription;
+   work();
   }
 }
 
 // This is where the GIFs come from
 
-// function setup(){
-//   noCanvas();
-//   var url = apiG + apiKeyG + query + description;
-//   loadJSON(url, gotData);
-// }
-//
-// function gotData(giphy){
-//   createImg(giphy.data[0].images.original.url);
-// }
+function work(){
+  var url = apiG + apiKeyG + query + description;
+  loadJSON(url, gotData);
+}
+
+function gotData(giphy){
+  createImg(giphy.data.image_url);
+}
